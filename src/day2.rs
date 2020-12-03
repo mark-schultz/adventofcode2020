7eff0264a618f995214946f25a3feb208f36c5de
Mark Schultz <mdschultz@eng.ucsd.edu>
2020-12-02 19:31:15 -0800

    Solve day 2

---
+++ src/day2.rs
@@ -0,0 +1,68 @@
+#[derive(Debug)]
+pub struct Password {
+    nums: Vec<u32>,
+    constrained_char: char,
+    password: String,
+}
+/// Input lines are of the form "X-Y c: abcd..."
+fn line_to_password(input: &str) -> Option<Password> {
+    let mut line = input.clone().split_whitespace();
+    let nums = line
+        .next()?
+        .split("-")
+        .map(|c| c.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    let constrained_char = line.next()?.chars().next()?;
+    let password = line.next()?;
+    Some(Password {
+        nums: nums,
+        constrained_char: constrained_char,
+        password: password.to_string(),
+    })
+}
+
+fn validate_password_range(input: &Password) -> bool {
+    assert!(input.nums.len() >= 2);
+    let num_range = std::ops::RangeInclusive::new(input.nums[0] as usize, input.nums[1] as usize);
+    num_range.contains(
+        &input
+            .password
+            .chars()
+            .filter(|c| c == &input.constrained_char)
+            .count(),
+    )
+}
+
+fn validate_password_indices(input: &Password) -> Option<bool> {
+    Some(
+        (input.password.chars().nth((input.nums[0] - 1) as usize)? == input.constrained_char)
+            ^ (input.password.chars().nth((input.nums[1] - 1) as usize)? == input.constrained_char),
+    )
+}
+
+fn none_is_false(input: Option<bool>) -> bool {
+    if let Some(i) = input {
+        i
+    } else {
+        false
+    }
+}
+
+pub fn parse_day2p1(input: &str) -> Vec<Password> {
+    input.lines().filter_map(|l| line_to_password(l)).collect()
+}
+
+pub fn solve_day2p1(input: &[Password]) -> usize {
+    input
+        .into_iter()
+        .filter(|p| validate_password_range(p))
+        .count()
+}
+
+pub fn solve_day2p2(input: &[Password]) -> usize {
+    input
+        .into_iter()
+        .map(|p| validate_password_indices(p))
+        .filter(|p| none_is_false(*p))
+        .count()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
+#![allow(dead_code)]
+
 mod day1;
+mod day2;
 #[macro_use]
 mod lib;
 
 fn main() {
-    /*
-    let mut prefix : String = "./inputs/".to_string();
-    prefix.push_str("day1p1");
-    prefix.push_str(".txt");
-    let data = std::fs::read_to_string(prefix).expect("Could not open file");
-    let mut parsed_data = day1::parse_day1p1(&data);
-    let solution = day1::solve_day1p1(&mut parsed_data);
-    println!("The answer is {}", solution.unwrap());
-    */
-    let data = std::fs::read_to_string("./inputs/day1p1.txt").expect("Could not find file.");
-    let mut parsed_data = day1::parse_day1(&data);
-    let sol = day1::solve_day1p1(&mut parsed_data);
-    assert_eq!(902451, sol.unwrap());
-    println!("The solution is {}", sol.unwrap())
+    let data = std::fs::read_to_string("./inputs/day2p1.txt").expect("Could not find file.");
+    let data = day2::parse_day2p1(&data);
+    let sol = day2::solve_day2p2(&data);
+    println!("The solution is: {}", sol)
 }
 
 #[cfg(test)]
@@ -35,5 +28,19 @@ mod tests {
         let mut parsed_data = day1::parse_day1(&data);
         let sol = day1::solve_day1p2(&mut parsed_data);
         assert_eq!(85555470, sol.unwrap());
+    }
+    #[test]
+    fn test_day2p1() {
+        let data = std::fs::read_to_string("./inputs/day2p1.txt").expect("Could not find file.");
+        let data = day2::parse_day2p1(&data);
+        let sol = day2::solve_day2p1(&data);
+        assert_eq!(sol, 416);
+    }
+    #[test]
+    fn test_day2p2() {
+        let data = std::fs::read_to_string("./inputs/day2p1.txt").expect("Could not find file.");
+        let data = day2::parse_day2p1(&data);
+        let sol = day2::solve_day2p2(&data);
+        assert_eq!(688, sol)
     }
 }
